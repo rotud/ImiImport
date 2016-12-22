b9c30bfdc13773100cc151e5ab204c116d58a109
tuklein <tuklein@TUKLEIN-JP3>
2016-12-22 17:16:39 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace ImiImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic obj = JObject.Parse(System.IO.File.ReadAllText(args[0]));
+            var items = obj?.lists?[0]?.list?.items;
+
+            Console.OutputEncoding = Encoding.UTF8;
+            foreach (var item in items)
+            {
+                if (item.type == 0)
+                {
+                    Console.Write(string.Join(", ", item.kanji.Count > 0 ? item.kanji : item.reading) + "\t");
+                    Console.Write(string.Join(", ", item.meaning?.eng) + "\t");
+                    Console.Write(string.Join(", ", item.reading) + "\n");
+                }
+                else if (item.type == 4)
+                {
+                    Console.Write(item.example.jap + "\t");
+                    Console.Write(item.example.eng + "\t\n");
+                }
+            }
+        }
+    }
+}
